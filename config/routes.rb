1565be5f5ebfd575a9cfa9f1cc310f819afe3722
Noel Delgado <noeldelgadom@myhost.local>
2017-06-01 17:29:44 -0500

    improved matches and teams index page

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   resources :matches
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'hiw#index'
+  root 'matches#index'
 end
